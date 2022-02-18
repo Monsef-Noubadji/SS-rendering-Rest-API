66769264d3b9dd7e829bd4edddf81c4c7ee1cfc0
monsef <noubadjimonsef@gmail.com>
2022-02-18 22:05:15 +0100

    Added source files

---
+++ App.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes").default;
+const AppError = require("./utils/appError");
+const errorHandler = require("./utils/errorHandler");
+
+app.use(api, router);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`The URL ${req.originalUrl} Does not exists`, 404));
+});
+app.use(errorHandler);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server up && running :) on port ${PORT}`);
+});
+
+module.exports = app;
