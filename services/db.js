a5d92f942617f88e6193ee4d4303138ded394aa4
Monsef Noubadji <67560520+Monsef-Noubadji@users.noreply.github.com>
2022-08-03 00:03:45 +0100

    added security layer

--- services/db.js
+++ services/db.js
@@ -1,9 +1,9 @@
-const mysql = require("mysql");
-const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "todos"
+const db = require("mysql");
+const conn = db.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER ,
+  password: process.env.PASSKEY,
+  database: process.env.DB
 });
 
 conn.connect();
